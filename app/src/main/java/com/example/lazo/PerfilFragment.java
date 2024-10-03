1f323bd37bf557964e81b96db614584b39d947c6
Michael López <michael.lopez16@inacapmail.cl>
2024-10-02 21:59:15 -0300

    fragmentos

--- app/src/main/java/com/example/lazo/PerfilFragment.java
+++ app/src/main/java/com/example/lazo/PerfilFragment.java
@@ -2,6 +2,7 @@ package com.example.lazo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -56,7 +57,7 @@ public class PerfilFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false);
